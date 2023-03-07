c2422aa89a3273d4df254e481beddeb8ba4b0ad1
Tarel-Github <sms_stmax@naver.com>
2023-03-07 13:36:03 +0900

    시작

---
+++ part1/studyPyQt/mp01_pyqtapp.py
@@ -0,0 +1,31 @@
+# PyQt 복습
+import sys
+from PyQt5.QtWidgets import *
+
+class qtApp(QWidget):# qwidget을 상속받음
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.lblMessage = QLabel('메시지', self)
+        self.lblMessage.setGeometry(10,10,300,50)
+
+        btnOk = QPushButton('OK', self)
+        btnOk.setGeometry(280, 250, 100, 40)
+        btnOk.clicked.connect(self.btnOk_clicked)
+
+        self.setGeometry(300, 200, 400, 300)
+        self.setWindowTitle('복습PyQt')
+        self.show()
+
+    def btnOk_clicked(self):
+        self.lblMessage.clear()
+        self.lblMessage.setText('메시지: 오케이!')
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = qtApp() # MyApp()
+    sys.exit(app.exec_())
