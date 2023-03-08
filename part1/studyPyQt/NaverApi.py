a8856ecce80e6cc9826f3eb84d39aefddad0ca3f
Tarel-Github <sms_stmax@naver.com>
2023-03-08 11:16:22 +0900

    네이버뉴스앱 제작 완료

--- part1/studyPyQt/NaverApi.py
+++ part1/studyPyQt/NaverApi.py
@@ -7,7 +7,7 @@ import json # 결과는 json으로
 class NaverApi:
     # 생성자
     def __init__(self) -> None:
-        print('Naver API 생성')
+        print(f'[{datetime.datetime.now()}]Naver API 생성')
 
     # Naver API를 요청 함수
     def get_request_url(self, url):
@@ -42,16 +42,21 @@ class NaverApi:
         else:
             return json.loads(retData) # json으로 return.
 
-    # json 데이터를 list로 변환
-    def get_post_data(self, post, outputs):
-        title = post['title']
-        description = post['description']
-        originallink = post['originallink']
-        link = post['link']
 
-        # 'Tue, 07 Mar 2023 17:04:00 +0900' 문자열로 들어온걸 날짜형으로 변경
-        pDate = datetime.datetime.striptime(post['pubDate'], '%a, %d, %b, %Y, %H:%M:%S +0900')
-        pubDate = pDate.strftime('%Y-%m-%d %H:%M:%S') # 2023-03-07 17:04:00 우리가 쓰는 형태로 변경
 
-        # outputs에 옮기기 - TO BE CONTINUED...
+    # # json 데이터를 list로 변환, 필요없어져서 주석처리
+    # def get_post_data(self, post, outputs) -> None:
+    #     title = post['title']
+    #     description = post['description']
+    #     originallink = post['originallink']
+    #     link = post['link']
+
+    #     # 'Tue, 07 Mar 2023 17:04:00 +0900' 문자열로 들어온걸 날짜형으로 변경
+    #     pDate = datetime.datetime.striptime(post['pubDate'], '%a, %d, %b, %Y, %H:%M:%S +0900')
+    #     pubDate = pDate.strftime('%Y-%m-%d %H:%M:%S') # 2023-03-07 17:04:00 우리가 쓰는 형태로 변경
+
+    #     # outputs에 옮기기 - TO BE CONTINUED...
+    #     outputs.append({'title':title, 'description':description,
+    #                     'originallink':originallink, 'link':link,
+    #                     'pubDate':pubDate})
     
