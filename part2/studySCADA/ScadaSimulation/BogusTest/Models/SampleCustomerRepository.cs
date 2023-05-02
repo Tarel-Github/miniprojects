066f34adf6cb465c88fba71ae9d1106cd6745146
Tarel-Github <sms_stmax@naver.com>
2023-05-02 14:01:20 +0900

    보거스 시작

---
+++ part2/studySCADA/ScadaSimulation/BogusTest/Models/SampleCustomerRepository.cs
@@ -0,0 +1,35 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BogusTest.Models
+{
+    public class SampleCustomerRepository
+    {
+        // 가짜고객 데이터 생성기
+        public IEnumerable<Customer> GetCustomers(int genNum)
+        {
+            Randomizer.Seed = new Random(123456);       // Seed갯수를 지정 123456은 마음대로
+            // 아래와 같은 규칙으로 주문 더미데이터를 생성 하겠다.
+            var orderGen = new Faker<Order>()
+                .RuleFor(g => g.Id, Guid.NewGuid) // ID값은 GUID로 자동 생성, 여기서 g는 자신이 원하는걸로 작성
+                .RuleFor(d => d.Date, f => f.Date.Past(3))      // 날짜를 3년전으로 셋팅 생성
+                .RuleFor(o => o.OrderValue, f => f.Finance.Amount(1, 10000)) // 1부터 10000까지 숫자중에서 렌덤하게 셋
+                .RuleFor(o => o.Shipped, f => f.Random.Bool(0.8f));          // 0.5f라면 true/false가 반환
+            // 고객더미데이터
+            var customerGen = new Faker<Customer>()
+                .RuleFor(c => c.Id, Guid.NewGuid())
+                .RuleFor(c => c.Name, f => f.Company.CompanyName())
+                .RuleFor(c => c.Address, f => f.Address.FullAddress())
+                .RuleFor(c => c.Phone, f => f.Phone.PhoneNumber())
+                .RuleFor(c => c.ContactName, f => f.Name.FullName())
+                .RuleFor(c => c.Orders, f => orderGen.Generate(f.Random.Number(1, 2)).ToList());
+
+            return customerGen.Generate(genNum);        // 10개의 가짜 고객데이터 생성, 리턴
+
+        }
+    }
+}
