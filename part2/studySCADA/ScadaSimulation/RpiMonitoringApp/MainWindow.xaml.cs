48451b225af8e1868136e39e9d131485e5711504
Tarel-Github <sms_stmax@naver.com>
2023-06-14 12:27:40 +0900

    MQTT연결

---
+++ part2/studySCADA/ScadaSimulation/RpiMonitoringApp/MainWindow.xaml.cs
@@ -0,0 +1,184 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using SmartHomeMonitoringApp.Views;
+using MahApps.Metro.Controls.Dialogs;
+using SmartHomeMonitoringApp.Logics;
+using System.ComponentModel;
+using ControlzEx.Theming;
+
+namespace SmartHomeMonitoringApp
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+
+        string DefaultTheme { get; set; } = "Light";    // 기본테마 Light
+        string DefaultAccent { get; set; } = "Cobalt";    // 기본색상 Cobalt
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.SyncWithAppMode;
+            ThemeManager.Current.SyncTheme();
+
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // <Frame> ==> Page.xaml
+            // <ContentControl> ==> UserControl.xaml
+            // ActiveItem.Content = new Views.DataBaseControl();
+        }
+
+        private void MnuExitSubsribe_Click(object sender, RoutedEventArgs e)
+        {
+            Process.GetCurrentProcess().Kill();  // 작업관리자에서 프로세스 종료! 이게 더 빠름
+            //Environment.Exit(0);    // 끝내기      // 0이 일반종료, 이외에도 코드가 많음, 종료 속도가 조금 느림
+        }
+
+        // MQTT 시작메뉴 클릭이벤트 핸들러
+        private void MnuStartSubsribe_Click(object sender, RoutedEventArgs e)
+        {
+            var mqttPopWin = new MqttPopupWindow();
+            mqttPopWin.Owner = this;
+            mqttPopWin.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            var result = mqttPopWin.ShowDialog();
+
+            if(result == true)
+            {
+                var userControl = new Views.DataBaseControl();
+                ActiveItem.Content = userControl;
+                StsSelScreen.Content = "DataBase Monitoring";
+            }
+        }
+
+        private async void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;        // e. Cancel을 true 하고 시작, 이거 없으면 이벤트가 발생하지 않는다.
+            var mySettings = new MetroDialogSettings 
+                                {
+                                    AffirmativeButtonText = "끝내기",
+                                    NegativeButtonText = "취소",
+                                    AnimateShow = true,
+                                    AnimateHide = true,
+            };
+            
+
+            var result = await this.ShowMessageAsync("프로그램 끝내기", "프로그램을 끝내시겠습니까?", 
+                                                     MessageDialogStyle.AffirmativeAndNegative, mySettings);
+            if(result == MessageDialogResult.Negative) 
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                if(Commons.MQTT_CLIENT != null && Commons.MQTT_CLIENT.IsConnected)
+                {
+                    Commons.MQTT_CLIENT.Disconnect();
+                }
+                Process.GetCurrentProcess().Kill();     // 가장 확실
+            }
+
+        }
+
+        // 창을 닫을 때 사용하는 MetroWindow_Closing 이벤트를 여기서 불러온다.
+        private void BtnExitProgram_Click(object sender, RoutedEventArgs e)
+        {
+            this.MetroWindow_Closing(sender, new CancelEventArgs());
+        }
+
+        private void MnuDataBaseMon_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveItem.Content = new Views.DataBaseControl();
+            StsSelScreen.Content = "DataBase Monitioring";
+        }
+
+        private void MnuRealTimeMon_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveItem.Content = new Views.RealTimeControl();
+            StsSelScreen.Content = "RealTimeControl View";
+        }
+
+        private void MnuVisualizationMon_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveItem.Content = new Views.VisualizationControl();
+            StsSelScreen.Content = "Visualization View";
+        }
+
+        private void MnuAbout_Click(object sender, RoutedEventArgs e)
+        {
+            var about = new About();
+            about.Owner = this;
+            about.ShowDialog();
+        }
+
+        // 모든 테마와 액센트를 전부 처리할 이벤트 핸들러
+        private void MnuThemeAccent_Checked(object sender, RoutedEventArgs e)
+        {
+            // 클릭되는 테마가 라이트인지 다크인지 판단/라이트를 클릭하면 다크는 체크해제, 다크를 클릭하면 라이트를 체크해제
+
+
+            // 액센트도 체크를 하는 값을 , 나머지 액센트 전부 체크 해제
+            switch((sender as MenuItem).Header)
+            {
+                case "Light":
+                    MnuLightTheme.IsChecked = true;
+                    MnuDarkTheme.IsChecked = false;
+                    DefaultTheme = "Light";
+                    break;
+                case "Dark":
+                    MnuLightTheme.IsChecked = false;
+                    MnuDarkTheme.IsChecked = true;
+                    DefaultTheme = "Dark";
+                    break;
+                case "Amber":
+                    MnuAccentAmber.IsChecked = true;
+                    MnuAccentBlue.IsChecked = false;
+                    MnuAccentBrown.IsChecked = false;
+                    MnuAccentCobalt.IsChecked = false;
+                    DefaultAccent = "Amber";
+                    break;
+                case "Blue":
+                    MnuAccentAmber.IsChecked = false;
+                    MnuAccentBlue.IsChecked = true;
+                    MnuAccentBrown.IsChecked = false;
+                    MnuAccentCobalt.IsChecked = false;
+                    DefaultAccent = "Blue";
+                    break;
+                case "Brown":
+                    MnuAccentAmber.IsChecked = false;
+                    MnuAccentBlue.IsChecked = false;
+                    MnuAccentBrown.IsChecked = true;
+                    MnuAccentCobalt.IsChecked = false;
+                    DefaultAccent = "Brown";
+                    break;
+                case "Cobalt":
+                    MnuAccentAmber.IsChecked = false;
+                    MnuAccentBlue.IsChecked = false;
+                    MnuAccentBrown.IsChecked = false;
+                    MnuAccentCobalt.IsChecked = true;
+                    DefaultAccent = "Cobalt";
+                    break;
+            }
+
+
+            ThemeManager.Current.ChangeTheme(this, $"{DefaultTheme}.{DefaultAccent}");
+        }
+    }
+}
---
+++ part2/studySCADA/ScadaSimulation/RpiMonitoringApp/Views/DataBaseControl.xaml.cs
@@ -0,0 +1,212 @@
+﻿using MahApps.Metro.Controls;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using SmartHomeMonitoringApp.Logics;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace SmartHomeMonitoringApp.Views
+{
+    /// <summary>
+    /// DataBaseControl.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class DataBaseControl : UserControl
+    {
+        public bool IsConnected { get; set; }
+
+        Thread MqttThread { get; set; } // 이게 없으면 UI 컨트롤이 어려워진다.
+
+        // MQTT Subscribiton text 과도문제로 속도저하를 잡기위해 변수
+        // 23.05.11 09:29 SMG
+
+        int MaxCount { get; set; } = 50;
+
+        public DataBaseControl()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            TxbBrokerUrl.Text = Commons.BROKERHOST;
+            TxbMqttTopic.Text = Commons.MQTTTOPIC;
+            TxtConnString.Text = Commons.MYSQL_CONNSTRING;
+
+            IsConnected = false;        // 아직 접속이 안되었음
+            BtnConnDb.IsChecked = false;
+
+            if (Commons.MQTT_CLIENT != null && Commons.MQTT_CLIENT.IsConnected)
+            {
+                IsConnected = true;
+                BtnConnDb.Content = "DB 연결중...";
+                BtnConnDb.IsChecked = true;
+                Commons.MQTT_CLIENT.MqttMsgPublishReceived += MQTT_CLIENT_MqttMsgPublishReceived;
+            }
+        }
+
+        // 토글버튼 체크(1: 접속/2: 접속끊기) 이벤트 핸들러
+        private void BtnConnDb_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectDB();
+        }
+
+        private void ConnectDB()
+        {
+            if (IsConnected == false)
+            {
+                // Mqtt 브로커를 생성
+                Commons.MQTT_CLIENT = new uPLibrary.Networking.M2Mqtt.MqttClient(Commons.BROKERHOST);
+
+                try
+                {
+                    // Mqtt subscribe(구독할) 로직
+                    if (Commons.MQTT_CLIENT.IsConnected == false)
+                    {
+                        // Mqtt 접속
+                        Commons.MQTT_CLIENT.MqttMsgPublishReceived += MQTT_CLIENT_MqttMsgPublishReceived;
+                        Commons.MQTT_CLIENT.Connect("MONITOR"); // ClientId = 모니터
+                        Commons.MQTT_CLIENT.Subscribe(new string[] { Commons.MQTTTOPIC },
+                                new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });    // QOS는 네트워크 통신에 옵션
+
+                        UpdateLog(">>> MQTT Broker Connected");
+
+                        BtnConnDb.IsChecked = true;
+                        IsConnected = true;     // 예외발생하면 true로 변경할 필요 없음
+                        BtnConnDb.Content = "MQTT 연결중...";
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    UpdateLog($"!!! MQTT Error 발생 : {ex.Message}");
+
+                    // Pass
+                }
+
+            }
+            else
+            {
+                try
+                {
+                    if (Commons.MQTT_CLIENT.IsConnected)
+                    {
+
+                        Commons.MQTT_CLIENT.MqttMsgPublishReceived -= MQTT_CLIENT_MqttMsgPublishReceived;
+                        Commons.MQTT_CLIENT.Disconnect();
+
+                        UpdateLog(">>> MQTT Broker DisConnected...");
+
+                        BtnConnDb.IsChecked = false;
+                        IsConnected= false;
+                        BtnConnDb.Content = "Connect";
+                    }
+
+
+                    BtnConnDb.IsChecked = false;
+                    IsConnected = false;
+                }
+                catch (Exception ex)
+                {
+
+                    UpdateLog($"!!! MQTT Error 발생 : {ex.Message}");
+                }
+
+                BtnConnDb.IsChecked = false;
+                IsConnected = false;
+            }
+        }
+
+        private void UpdateLog(string msg)
+        {
+            // 예외처리 필요
+            this.Invoke(() => {
+                if(MaxCount <= 0)
+                {
+                    TxtLog.Text = string.Empty;
+                    TxtLog.Text += ">>> 문서 건수가 많아져서 초기화! \n";
+                    TxtLog.ScrollToEnd();
+                    MaxCount = 50;  // 테스트할땐 10, 운영시는 50
+                }
+
+                TxtLog.Text += $"{msg}\n";
+                TxtLog.ScrollToEnd();
+                MaxCount--;
+            });
+        }
+
+        // Subscribe가 발생할 때 이벤트핸들러
+        private void MQTT_CLIENT_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            var msg = Encoding.UTF8.GetString(e.Message);
+            UpdateLog(msg);
+            SetToDataBase(msg, e.Topic);        // 실제 DB에 저장처리
+        }
+
+        // DB 저장처리 메서드
+        private void SetToDataBase(string msg, string topic)
+        {
+            var currValue = JsonConvert.DeserializeObject<Dictionary<string, string>>(msg);
+            if (currValue != null)
+            {
+                Debug.WriteLine(currValue["DEV_ID"]);
+                Debug.WriteLine(currValue["TYPE"]);
+                Debug.WriteLine(currValue["CURR_DT"]);
+                Debug.WriteLine(currValue["STAT"]);
+                // living
+                var tmp = currValue["STAT"].Split('|'); // 29.0 | 45.0 잘라준 다음
+                var temp = tmp[0].Trim();   // "29.0" trim() 공백제거
+                var humid = tmp[1].Trim();  // "45.0" trim() 공백제거
+
+                try
+                {
+                    using (MySqlConnection conn = new MySqlConnection(Commons.MYSQL_CONNSTRING)) 
+                    { 
+                        if (conn.State == System.Data.ConnectionState.Closed) conn.Open();
+                        string insQuery = @"INSERT INTO miniproject.smarthomesensor
+                                                  (Home_Id, Room_Name, Sensing_DateTime, Temp, Humid)
+                                                  VALUES
+                                                  (@Home_Id, @Room_Name, @Sensing_DateTime, @Temp, @Humid )";
+                        
+                        MySqlCommand cmd = new MySqlCommand(insQuery, conn);
+                        cmd.Parameters.AddWithValue("@Home_Id", currValue["Home_Id"]);
+                        cmd.Parameters.AddWithValue("@Room_Name", currValue["Room_Name"]);
+                        cmd.Parameters.AddWithValue("@Sensing_DateTime", currValue["Sensing_DateTime"]);
+                        cmd.Parameters.AddWithValue("@Temp", temp);
+                        cmd.Parameters.AddWithValue("@Humid", humid);
+
+                        // ....파라미터 다섯개
+                        if (cmd.ExecuteNonQuery() == 1) 
+                        {
+                            UpdateLog(">>> DB Insert succeed.");
+                        }
+                        else
+                        {
+                            UpdateLog(">>> DB Insert failed.");  // 일어날 일이 거의 없음
+                        }
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    UpdateLog($"!!! DB Error 발생 : {ex.Message}");
+                }
+
+            }
+        }
+    }
+}
